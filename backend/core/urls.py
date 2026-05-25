a902f1a132cdc926961a90b3c57a2257e84b3042
Kanhaiya-byte <kanhaiyayadav0003@gmail.com>
2026-05-25 20:11:24 +0530

    Added backend home route

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-
 from django.urls import path, include
+from django.http import JsonResponse
+
+
+def home(request):
+    return JsonResponse({
+        "message": "ESG Backend API Running Successfully"
+    })
+
 
 urlpatterns = [
-
+    path('', home),
     path('admin/', admin.site.urls),
-
     path('api/', include('emissions.urls')),
-
 ]
